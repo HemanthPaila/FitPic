05c8824c4d967464ef459bfc94ce764df3dce18d
paila.hemanth <74002716+HemanthPaila@users.noreply.github.com>
2026-05-04 15:10:05 -0400

    Implement fit prediction service with preprocessing

---
+++ fit_service.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tensorflow as tf
+
+model = tf.keras.models.load_model("fit_model.h5")
+
+def preprocess(data):
+    data = np.array(data, dtype=np.float32)
+    data[0:5] /= 200.0
+    return data.reshape(1, -1)
+
+def predict_fit(features):
+    x = preprocess(features)
+    
+    fit_score, size_pred = model.predict(x)
+    
+    sizes = ["S", "M", "L", "XL"]
+    
+    return {
+        "fit_score": float(fit_score[0][0]),
+        "size": sizes[np.argmax(size_pred)]
+    }
